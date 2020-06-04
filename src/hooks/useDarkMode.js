a4fd8447ee12a29a5893a7bcd3a87f2440f7be3e
Michael Sheets <michaelsheets@MacBook-Pro.local>
2020-06-03 21:51:35 -0500

    mvp met

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react'
+import { useLocalStorage} from './useLocalStorage'
+
+export const useDarkMode = (key) => {
+    const [darkmode, setDarkMode] = useLocalStorage(key);
+    useEffect(() => {
+        if (darkmode === true) {
+            document.body.classList.add('dark-mode')
+        }
+        else {
+            document.body.classList.remove('dark-mode')
+        }
+    }, [darkmode])
+    return [darkmode, setDarkMode]
+};
